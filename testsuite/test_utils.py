8f2e637076acb2694e92340682473139c8f97e8f
Oleg Gaidukov <ogaidukov@gmail.com>
2019-09-19 21:57:17 +0300

    utils update

---
+++ testsuite/test_utils.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from ton_client import utils as tc_utils
+
+
+class UtilsTestCase(unittest.TestCase):
+    def test_raw_to_userfriendly(self):
+        raw = '-1:8156775b79325e5d62e742d9b96c30b6515a5cd2f1f64c5da4b193c03f070e0d'
+        uf = 'Ef+BVndbeTJeXWLnQtm5bDC2UVpc0vH2TF2ksZPAPwcODSkb'
+
+        uf_test = tc_utils.raw_to_userfriendly(raw)
+        self.assertEqual(uf, uf_test)
+
+    def test_userfriendly_to__raw(self):
+        raw = '-1:8156775b79325e5d62e742d9b96c30b6515a5cd2f1f64c5da4b193c03f070e0d'
+        uf = 'Ef+BVndbeTJeXWLnQtm5bDC2UVpc0vH2TF2ksZPAPwcODSkb'
+
+        raw_test = tc_utils.userfriendly_to_raw(uf)
+        self.assertEqual(raw, raw_test)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- ton_client/utils.py
+++ ton_client/utils.py
@@ -2,6 +2,9 @@
 
 import base64
 import asyncio
+import struct
+
+import crc16
 
 
 def key_b64_to_hex_flip(b64_key):
@@ -12,3 +15,23 @@ def key_b64_to_hex_flip(b64_key):
 
 def coro_result(coro):
     return asyncio.get_event_loop().run_until_complete(coro)
+
+
+def raw_to_userfriendly(address, tag=0x11):
+    workchain_id, key = address.split(':')
+    workchain_id = int(workchain_id)
+    key = bytearray.fromhex(key)
+
+    short_ints = [j * 256 + i for i, j in zip(*[iter(key)] * 2)]
+    payload = struct.pack(f'bb{"H"*16}', tag, workchain_id, *short_ints)
+    crc = crc16.crc16xmodem(payload)
+
+    e_key = payload + struct.pack('>H', crc)
+    return base64.b64encode(e_key).decode("utf-8")
+
+
+def userfriendly_to_raw(address):
+    k = base64.b64decode(address)[1:34]
+    workchain_id = struct.unpack('b', k[:1])[0]
+    key = k[1:].hex()
+    return f'{workchain_id}:{key}'
